99d0dfc3e9be6c13762884c221cfc1570330e787
Tarun Velli <tarunvelli@gmail.com>
2017-06-30 05:14:58 +0530

    removed switch and wrapped commands in functions

--- index.js
+++ index.js
@@ -5,23 +5,40 @@ var key = require('./key/key.js')
 var type = process.argv[2]
 var word = process.argv[3]
 
-switch(type) {
-  case "syn" :
+// Command definitions
+
+var commands = {}
+
+commands["syn"] = function() {
   url = 'http://api.wordnik.com:80/v4/word.json/'+word+'/relatedWords?relationshipTypes=synonym&api_key='+key
-  break
-  case "ant":
-  url = 'http://api.wordnik.com:80/v4/word.json/'+word+'/relatedWords?relationshipTypes=antonym&api_key='+key
-  break
-  case "def":
-  url = 'http://api.wordnik.com:80/v4/word.json/'+word+'/definitions?limit=5&api_key='+key
-  break
-  case "ex":
-  url = 'http://api.wordnik.com:80/v4/word.json/'+word+'/examples?limit=5&api_key='+key
+  getData(url)
 }
 
+commands["ant"] = function() {
+  url = 'http://api.wordnik.com:80/v4/word.json/'+word+'/relatedWords?relationshipTypes=antonym&api_key='+key
+  getData(url)
+}
 
-http.request( url, function(res) {
-  res.on('data', function (info) {
-    console.log(JSON.parse(info))
-  });
-}).end();
+commands["def"] = function() {
+  url = 'http://api.wordnik.com:80/v4/word.json/'+word+'/definitions?limit=5&api_key='+key
+  getData(url)
+}
+
+commands["ex"] = function() {
+  url = 'http://api.wordnik.com:80/v4/word.json/'+word+'/examples?limit=5&api_key='+key
+  getData(url)
+}
+
+// http GET request
+
+function getData() {
+  http.request( url, function(res) {
+    res.on('data', function (info) {
+      console.log(JSON.parse(info))
+    });
+  }).end();
+}
+
+// execute command
+
+commands[type]()
