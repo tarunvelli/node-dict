2bef4ef1368807f8107da3b688f53eecbf1819e5
Tarun Velli <tarunvelli@gmail.com>
2017-06-30 06:27:00 +0530

    modified geData() function with a callback

--- index.js
+++ index.js
@@ -16,28 +16,33 @@ if (type==null) {
 var commands = {}
 
 commands["syn"] = function() {
-  url = 'http://api.wordnik.com:80/v4/word.json/'+word+'/relatedWords?relationshipTypes=synonym&api_key='+key
-  getData(url)
+  url = '/v4/word.json/'+word+'/relatedWords?relationshipTypes=synonym&api_key='+key
+  getData(url,print)
 }
 
 commands["ant"] = function() {
-  url = 'http://api.wordnik.com:80/v4/word.json/'+word+'/relatedWords?relationshipTypes=antonym&api_key='+key
-  getData(url)
+  url = '/v4/word.json/'+word+'/relatedWords?relationshipTypes=antonym&api_key='+key
+  getData(url,print)
 }
 
 commands["def"] = function() {
-  url = 'http://api.wordnik.com:80/v4/word.json/'+word+'/definitions?limit=5&api_key='+key
-  getData(url)
+  url = '/v4/word.json/'+word+'/definitions?limit=5&api_key='+key
+  getData(url,print)
 }
 
 commands["ex"] = function() {
-  url = 'http://api.wordnik.com:80/v4/word.json/'+word+'/examples?limit=5&api_key='+key
-  getData(url)
+  url = '/v4/word.json/'+word+'/examples?limit=5&api_key='+key
+  getData(url,print)
 }
 
 commands["wotd"] = function() {
-  url = 'http://api.wordnik.com:80/v4/words.json/wordOfTheDay?date='+date+'&api_key='+key
-  getData(url)
+  url = '/v4/words.json/wordOfTheDay?date='+date+'&api_key='+key
+  getData(url,callback)
+
+  function callback(info) {
+    word = JSON.parse(info)["word"]
+    commands["dict"]()
+  }
 }
 
 commands["dict"] = function() {
@@ -50,12 +55,28 @@ commands["dict"] = function() {
 
 // http GET request
 
-function getData() {
-  http.request( url, function(res) {
+function getData(url, print) {
+
+  var options = {
+    host : 'api.wordnik.com',
+    port : 80,
+    path : url,
+    method : 'GET'
+  };
+
+  http.request(options, function(res) {
     res.on('data', function (info) {
-      console.log(JSON.parse(info))
+      print(info)
     });
   }).end();
+
+}
+
+// printing function
+
+function print(info) {
+  info = JSON.parse(info)
+  console.log(info)
 }
 
 // execute command
