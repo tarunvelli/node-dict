4220838bcb9c0e5ae04a6999e79f6da371320fbd
tarunvelli <tarunvelli@gmail.com>
2017-09-18 13:23:44 +0530

    refactoring 1.0

--- dict.js
+++ dict.js
@@ -1,81 +1,92 @@
 #!/usr/bin/env node
-var axios = require('axios')
-var readline = require('readline')
-var key = require('./key/key.js')
-var date = new Date()
-date = `${date.getUTCFullYear()}-${(date.getMonth() + 1)}-${(date.getDate())}`
+const axios = require('axios')
+const readline = require('readline')
+const key = require('./key/key.js')
 
-var type = process.argv[2]
-var word = process.argv[3]
-
-if (type == null) {
-  type = 'wotd' // word of the day
-}
+const type = process.argv[2] || 'wotd'
+const queryWord = process.argv[3]
 
 // Command definitions
 
 var commands = {}
 
-commands['syn'] = async () => {
-  var url = `/v4/word.json/${word}/relatedWords?relationshipTypes=synonym&api_key=${key}`
-  var txt = `Synonyms for '${word}' are :\n`
+commands['syn'] = async (parsedWord) => {
+  var url = `/v4/word.json/${parsedWord}/relatedWords?relationshipTypes=synonym&api_key=${key}`
+  var txt = `Synonyms for '${parsedWord}' are :\n`
+
   await getData(url)
-  .then(response => processData(response.data, txt, 'syn'))
+  .then(response => processData(response.data, 'syn'))
+  .then(response => print(response, txt, 'syn'))
   .catch(err => console.log(err))
+
   return Promise.resolve()
 }
 
-commands['ant'] = async () => {
-  var url = `/v4/word.json/${word}/relatedWords?relationshipTypes=antonym&api_key=${key}`
-  var txt = `Antonyms for '${word}' are :\n`
+commands['ant'] = async (parsedWord) => {
+  var url = `/v4/word.json/${parsedWord}/relatedWords?relationshipTypes=antonym&api_key=${key}`
+  var txt = `Antonyms for '${parsedWord}' are :\n`
+
   await getData(url)
-  .then(response => processData(response.data, txt, 'ant'))
+  .then(response => processData(response.data, 'ant'))
+  .then(response => print(response, txt, 'ant'))
   .catch(err => console.log(err))
+
   return Promise.resolve()
 }
 
-commands['def'] = async () => {
-  var url = `/v4/word.json/${word}/definitions?limit=5&api_key=${key}`
-  var txt = `Definitions for '${word}' are :\n`
+commands['def'] = async (parsedWord) => {
+  var url = `/v4/word.json/${parsedWord}/definitions?limit=5&api_key=${key}`
+  var txt = `Definitions for '${parsedWord}' are :\n`
+
   await getData(url)
-  .then(response => processData(response.data, txt, 'def'))
+  .then(response => processData(response.data, 'def'))
+  .then(response => print(response, txt, 'def'))
   .catch(err => console.log(err))
+
   return Promise.resolve()
 }
 
-commands['ex'] = async () => {
-  var url = `/v4/word.json/${word}/examples?limit=5&api_key=${key}`
-  var txt = `Examples for '${word}' are :\n`
+commands['ex'] = async (parsedWord) => {
+  var url = `/v4/word.json/${parsedWord}/examples?limit=5&api_key=${key}`
+  var txt = `Examples for '${parsedWord}' are :\n`
+
   await getData(url)
-  .then(response => processData(response.data, txt, 'ex'))
+  .then(response => processData(response.data, 'ex'))
+  .then(response => print(response, txt, 'ex'))
   .catch(err => console.log(err))
+
   return Promise.resolve()
 }
 
 commands['wotd'] = async () => {
+  var date = new Date()
+  date = `${date.getUTCFullYear()}-${(date.getMonth() + 1)}-${(date.getDate())}`
+
   var url = `/v4/words.json/wordOfTheDay?date=${date}&api_key=${key}`
   var txt = 'Word of the day is : '
+  var parsedWord
+
   await getData(url)
   .then(response => {
-    word = response.data.word
-    console.log(`${txt} ${word}\n`)
+    parsedWord = response.data.word
+    console.log(`${txt} ${parsedWord}\n`)
   })
   .catch(err => console.log(err))
 
-  commands['dict']()
+  commands['dict'](parsedWord)
 }
 
-commands['dict'] = async () => {
-  await commands['def']()
-  await commands['syn']()
-  await commands['ant']()
-  await commands['ex']()
+commands['dict'] = async (parsedWord) => {
+  await commands['def'](parsedWord)
+  await commands['syn'](parsedWord)
+  await commands['ant'](parsedWord)
+  await commands['ex'](parsedWord)
 }
 
 commands['play'] = () => {
   var url = `/v4/words.json/randomWord?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=2&maxLength=8&api_key=${key}`
   getData(url)
-  .then(response => playFunction(response.data))
+  .then(response => playFunction(response.data.word))
   .catch(err => console.log(err))
 }
 
@@ -89,17 +100,20 @@ var dir = {
   'ant': []
 }
 
-async function playFunction (info) {
-  word = info.word
+async function playFunction (randomWord) {
   dir = {
     'def': [],
     'syn': [],
     'ant': []
   }
-  await commands['def']()
-  await commands['syn']()
-  await commands['ant']()
-  gameOn()
+
+  await Promise.all([
+    commands['def'](randomWord),
+    commands['syn'](randomWord),
+    commands['ant'](randomWord)
+  ])
+
+  gameOn(true, randomWord)
 }
 
 // main game
@@ -108,55 +122,46 @@ var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 })
-var newHint = true
-var clue
 
-function gameOn () {
-  clue = newHint ? hinter() : ''
-  newHint = false
+function gameOn (showHint, randomWord) {
+  var clue = showHint ? hinter(randomWord) : ''
 
-  rl.question(`\n${clue} \nGuess the word: `, function (answer) {
-    if (answer === word || dir['syn'].includes(answer)) {
+  rl.question(`\n${clue} \nGuess the word: `, answer => {
+    if (answer === randomWord || dir['syn'].includes(answer)) {
       console.log('You guessed right!')
       rl.close()
     } else {
       console.log('\nYou guessed wrong :/\n\n1: Guess again\n2: New hint\n3: Show word and quit')
-      wrongGuess()
+      wrongGuess(randomWord)
     }
   })
 }
 
 // options displayed on wrong guess
 
-function wrongGuess () {
+function wrongGuess (randomWord) {
   rl.question('Enter your option\n', function (answer) {
     if (answer === '1') {
       console.log('Guess again')
-      gameOn()
+      gameOn(false, randomWord)
     } else if (answer === '2') {
       console.log('New hint')
-      newHint = true
-      gameOn()
+      gameOn(true, randomWord)
     } else if (answer === '3') {
       console.log('Show word and quit')
-      console.log(word)
+      console.log(randomWord)
       rl.close()
     } else {
       console.log('Invalid option')
-      wrongGuess()
+      wrongGuess(randomWord)
     }
   })
 }
 
 // hint generator
 
-function hinter () {
-  var coinFlip
-  if (clue == null) {
-    coinFlip = 1
-  } else {
-    coinFlip = Math.floor(Math.random() * 2)
-  }
+function hinter (parsedWord) {
+  var coinFlip = Math.floor(Math.random() * 2)
 
   if (coinFlip) {
     do {
@@ -166,7 +171,7 @@ function hinter () {
     var index2 = Math.floor(Math.random() * row.length)
     return `${hintsLong[index1]} : ${row[index2]}`
   } else {
-    return `Jumbled word : ${jumble(word)}`
+    return `Jumbled word : ${jumble(parsedWord)}`
   }
 }
 
@@ -186,19 +191,17 @@ function jumble (word) {
 
 // GET request
 
-function getData (url, next, txt, id) {
-  var options = {
+function getData (url) {
+  return axios({
     url: url,
     baseURL: 'http://api.wordnik.com',
     method: 'get'
-  }
-
-  return axios(options)
+  })
 }
 
 // data formatting
 
-function processData (response, txt, id) {
+function processData (response, id) {
   if (id === 'ex') {
     response = response.examples.map(example => example.text)
   } else if (id === 'def') {
@@ -207,19 +210,28 @@ function processData (response, txt, id) {
     response = response[0].words
   }
 
-  if (type === 'play') {
-    dir[id] = response
-  } else {
-    txt = txt + response.reduce((acc, cur, index) => acc + `${index + 1}) ${cur}\n`, '\n')
-    console.log(txt)
+  if (type !== 'play') {
+    response = response.reduce((acc, cur, index) => acc + `${index + 1}) ${cur}\n`, '\n')
     rl.close()
+  }
+
+  return Promise.resolve(response)
+}
+
+// print function
+
+function print (response, txt, id) {
+  if (type !== 'play') {
+    console.log(txt + response)
+  } else {
+    dir[id] = response
   }
 }
 
 // execute command
 
 if (typeof commands[type] === 'function') {
-  commands[type]()
+  commands[type](queryWord)
 } else {
   console.log('Not a valid option')
   rl.close()
